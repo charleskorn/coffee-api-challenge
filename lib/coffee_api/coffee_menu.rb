7a9e47ae63997709bff85b7b42b746619fff908e
Charles Korn <ckorn@thoughtworks.com>
2016-03-21 16:57:26 +1100

    Add realistic coffee menu. (First contract passing.)

--- lib/coffee_api/coffee_menu.rb
+++ lib/coffee_api/coffee_menu.rb
@@ -1,14 +1,40 @@
 module CoffeeAPI
   class CoffeeMenu
+    # rubocop:disable Metrics/MethodLength
     def coffees
-      item = {
-        name: 'Coffee',
-        price: 4,
-        caffeine_level: 2,
-        milk_ratio: 2
-      }
-
-      [item, item, item, item]
+      [
+        {
+          name: 'long black',
+          price: 3,
+          caffeine_level: 8,
+          milk_ratio: 0
+        },
+        {
+          name: 'flat white',
+          price: 3.5,
+          caffeine_level: 5,
+          milk_ratio: 2
+        },
+        {
+          name: 'latte',
+          price: 3.5,
+          caffeine_level: 5,
+          milk_ratio: 3
+        },
+        {
+          name: 'espresso',
+          price: 2,
+          caffeine_level: 10,
+          milk_ratio: 0
+        },
+        {
+          name: 'machiato',
+          price: 2.5,
+          caffeine_level: 10,
+          milk_ratio: 0.5
+        }
+      ]
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
