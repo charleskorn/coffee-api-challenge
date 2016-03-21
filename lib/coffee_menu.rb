53d7e6d17598ab5f09a83f92cc2d32a60e092bb6
Charles Korn <ckorn@thoughtworks.com>
2016-03-21 15:16:32 +1100

    More coffee attributes.

--- lib/coffee_menu.rb
+++ lib/coffee_menu.rb
@@ -2,7 +2,9 @@ class CoffeeMenu
   def menu_items
     item = {
       name: 'Coffee',
-      price: 4
+      price: 4,
+      caffeine_level: 2,
+      milk_ratio: 2
     }
 
     [item, item, item, item]
--- spec/coffee_menu_spec.rb
+++ spec/coffee_menu_spec.rb
@@ -3,8 +3,8 @@ require 'coffee_menu'
 
 describe CoffeeMenu do
   describe '#menu_items' do
-    it 'returns a list of coffees' do
-      expect(subject.menu_items.length).to eq(4)
+    it 'returns a list of at least four coffees' do
+      expect(subject.menu_items.length).to be >= 4
     end
 
     it 'has names for each of the coffees' do
@@ -13,6 +13,14 @@ describe CoffeeMenu do
 
     it 'has prices for each of the coffees' do
       expect(subject.menu_items).to each_have_key(:price)
+    end
+
+    it 'has caffeine levels for each of the coffees' do
+      expect(subject.menu_items).to each_have_key(:caffeine_level)
+    end
+
+    it 'has milk ratios for each of the coffees' do
+      expect(subject.menu_items).to each_have_key(:milk_ratio)
     end
   end
 end
